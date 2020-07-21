5f37a8a308dba5a60c2dbcfadd06c32d987ab807
roman dod <stalkeroma1@mail.ru>
2020-07-21 22:31:53 +0300

    add swipe gesture recognize

--- MoveCircle/ViewController.swift
+++ MoveCircle/ViewController.swift
@@ -24,6 +24,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        
+        leftSwipe.direction = .left
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        
+        rightSwipe.direction = .right
+        
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        
+        upSwipe.direction = .up
+        
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        
+        downSwipe.direction = .down
+        
+        
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(downSwipe)
+        
+        
         
        let circleView = self.createCircleView()
        placeView.addSubview(circleView)
@@ -33,36 +56,83 @@ class ViewController: UIViewController {
         
     }
     
-    
     func createCircleView() -> UIView{
-        
-        let maxX: CGFloat = placeView.bounds.width - 100
-        let maxY: CGFloat = placeView.bounds.height - 100
-        let minX: CGFloat = placeView.bounds.width + 100
-        let minY: CGFloat = placeView.bounds.height + 100
-        
-        let circleView = UIView()
-        circleView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        circleView.backgroundColor = UIColor.init(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
-        circleView.layer.cornerRadius = 50
-        
-        
-        return circleView
-        
+    //
+    //        let maxX: CGFloat = placeView.bounds.width - 100
+    //        let maxY: CGFloat = placeView.bounds.height - 100
+    //        let minX: CGFloat = placeView.bounds.width + 100
+    //        let minY: CGFloat = placeView.bounds.height + 100
+            
+            let circleView = UIView()
+            circleView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            circleView.backgroundColor = UIColor.init(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+            circleView.layer.cornerRadius = 50
+            
+            
+            return circleView
+            
+        }
+    
+    func getDirectionText(_ direction: Direction) {
+             
+             switch direction {
+             case .down:
+                 messageLabel.text = "we're moving to down"
+             case .up:
+                 messageLabel.text = "we're moving to up"
+             case .left:
+                 messageLabel.text = "we're moving to the left"
+             case .right:
+                 messageLabel.text = "we're moving to the right"
+     
+         }
+     }
+    
+    @objc func handleSwipe(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            switch sender.direction {
+            case .right:
+                if circleViewGlobal.frame.origin.x + circleViewGlobal.frame.size.width < placeView.frame.size.width - moveLength{
+                    circleViewGlobal.frame.origin.x += CGFloat(moveLength)
+                    getDirectionText(.right)
+                    
+                } else {
+                    messageLabel.text = "Can't move"
+                }
+            case .left:
+                if circleViewGlobal.frame.origin.x > 0 + moveLength{
+                    circleViewGlobal.frame.origin.x -= CGFloat(moveLength)
+                    getDirectionText(.left)
+                    
+                } else {
+                    messageLabel.text = "Can't move"
+                }
+            case .up:
+                if circleViewGlobal.frame.origin.y >  0 + moveLength{
+                    circleViewGlobal.frame.origin.y -= CGFloat(moveLength)
+                    getDirectionText(.up)
+                } else{
+                    messageLabel.text = "Can't move"
+                }
+                
+            case .down:
+                if circleViewGlobal.frame.origin.y + circleViewGlobal.frame.size.height < placeView.frame.size.height - moveLength{
+                    circleViewGlobal.frame.origin.y += CGFloat(moveLength)
+                    getDirectionText(.down)
+                } else{
+                    messageLabel.text = "Can't move"
+                }
+                
+            
+            default:
+                break
+            }
     }
     
-//    func canMoveLeft() -> Bool {
-//
-//        if placeView.frame.origin.x + 100 > placeView.frame.size.width{
-//
-//            print("Cant Move")
-//            return true
-//
-//        }
-//
-//        return false
-//    }
     
+    
+    
+  /*
     @IBAction func pressedUpButton(_ sender: UIButton) {
 //        guard let circleViewGlobal = circleViewGlobal else {
 //            return
@@ -118,21 +188,9 @@ class ViewController: UIViewController {
         }
     }
     
-    func getDirectionText(_ direction: Direction) {
-            
-            switch direction {
-            case .down:
-                messageLabel.text = "we're moving to down"
-            case .up:
-                messageLabel.text = "we're moving to up"
-            case .left:
-                messageLabel.text = "we're moving to the left"
-            case .right:
-                messageLabel.text = "we're moving to the right"
-    
-        }
-    }
+  */
+ 
     
 
 }
-
+}
